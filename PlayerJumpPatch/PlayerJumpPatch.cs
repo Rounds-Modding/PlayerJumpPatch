751eae04344aecc81d356196081f1fee78982c93
pdcook <qphysicscook@gmail.com>
2021-06-29 21:50:37 -0400

    version bump

--- PlayerJumpPatch/PlayerJumpPatch.cs
+++ PlayerJumpPatch/PlayerJumpPatch.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace PlayerJumpPatch
 {
-        [BepInPlugin(ModId, ModName, "0.0.0.1")]
+        [BepInPlugin(ModId, ModName, "0.0.0.2")]
         [BepInProcess("Rounds.exe")]
         public class PlayerJumpPatch : BaseUnityPlugin
         {
